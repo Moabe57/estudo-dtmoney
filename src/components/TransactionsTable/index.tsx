83d4999bc5a610e148b100461c8af5af7073f4f2
Moabe Souza <moabe57@gmail.com>
2022-05-13 22:31:25 -0300

    primeiro commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desensolvimento</td>
+            <td className="deposit">R$ 12.000</td>
+            <td>Tirolesa</td>
+            <td>20/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$ 1.000</td>
+            <td>Casa</td>
+            <td>12/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
