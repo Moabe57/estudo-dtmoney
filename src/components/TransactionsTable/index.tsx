b42b25480ff35cd030d01c101f7af666fa1b67c3
Moabe Souza <moabe57@gmail.com>
2022-05-25 20:54:10 -0300

    25-05

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,24 +1,11 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useContext } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
-interface Transaction {
-  id: number
-  title: string
-  amount: number
-  type: string 
-  category: string
-  createdAt: string
-
-}
 
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
+  const {transactions} = useContext(TransactionsContext)
 
-  useEffect(() => {
-    api.get('transactions')
-      .then(response => setTransactions(response.data.transactions))
-  }, [])
 
   return (
     <Container>
@@ -33,8 +20,8 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          {transactions.map(transaction => {
-            return (
+          {transactions.map(transaction => (
+            
               <tr key={transaction.id}>
                 <td>{transaction.title}</td>
                 <td className={transaction.type}> {new Intl.NumberFormat('pt-BR', {
@@ -44,8 +31,8 @@ export function TransactionsTable() {
                 <td>{transaction.category}</td>
                 <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
               </tr>
-            )
-          })}
+            
+          ))}
         </tbody>
       </table>
     </Container>
